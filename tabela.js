7d92e366e769edbba62cd2dfcdc97ec870d93040
Felype Henrique <felypehenrique25@gmail.com>
2024-07-22 11:10:50 -0300

    tabela2024

---
+++ tabela.js
@@ -0,0 +1,225 @@
+const tabela2024 = [
+    [
+        {
+            nome: 'Atletico Goianiense',
+            sigla: 'ACG',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Athletico-PR',
+            sigla: 'CAP',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Atletico-MG',
+            sigla: 'CAM',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Bahia',
+            sigla: 'BAH',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Botafogo',
+            sigla: 'BOT',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Bragantino',
+            sigla: 'RBB',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Corinthians',
+            sigla: 'SCCP',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Criciuma',
+            sigla: 'CRI',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Cruzeiro',
+            sigla: 'CRU',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Cuiaba',
+            sigla: 'CUI',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Flamengo',
+            sigla: 'FLA',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Fluminense',
+            sigla: 'FLU',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Fortaleza',
+            sigla: 'FOR',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Gremio',
+            sigla: 'GRE',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Internacional',
+            sigla: 'INT',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Juventude',
+            sigla: 'JUV',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Palmeiras',
+            sigla: 'PAL',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Sao Paulo',
+            sigla: 'SAO',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Vasco',
+            sigla: 'VAS',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        },
+        {
+            nome: 'Vitoria',
+            sigla: 'ECV',
+            pontos: 0,
+            vitorias: 0,
+            empates: 0,
+            derrotas: 0,
+            golsMarcados: 0,
+            golsSofridos: 0,
+            saldoGols: 0
+        }
+    ]
+
+]
