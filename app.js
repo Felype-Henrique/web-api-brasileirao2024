36b810bec2eb8f20617f29e4fa02d15b1fc5cccb
Felype Henrique <felypehenrique25@gmail.com>
2024-07-23 09:49:48 -0300

    End point

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
     res.send(tabela2024);
 });
 
+app.get('/:sigla', (req, res) => {
+    const siglaInformada = req.params.sigla.toLocaleUpperCase();
+    const time = tabela2024.find(infoTime => infoTime.sigla === siglaInformada);
+    res.send(time);
+});
+
 app.listen(300, () => {
     console.log("Servidor rodando com sucesso")
 });
