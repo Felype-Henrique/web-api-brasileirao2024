1353015800d099ab22f0d33f041a9bd443f92d29
Felype Henrique <felypehenrique25@gmail.com>
2024-07-23 09:32:06 -0300

    express

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-import tabela2024 from "./tabela";
+import tabela2024 from './tabela.js';
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(tabela2024);
+});
+
+app.listen(300, () => {
+    console.log("Servidor rodando com sucesso")
+});
