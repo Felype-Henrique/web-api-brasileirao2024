697989b5ebaf0e409fda9b42bf6fc27cf30ef1da
Felype Henrique <felypehenrique25@gmail.com>
2024-07-23 10:15:42 -0300

    Status Code

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ import express from 'express'
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(tabela2024);
+    res.status(200).send(tabela2024);
 });
 
 app.get('/:sigla', (req, res) => {
     const siglaInformada = req.params.sigla.toLocaleUpperCase();
     const time = tabela2024.find(infoTime => infoTime.sigla === siglaInformada);
-    res.send(time);
+    if (!time) { // undefined -> se comporta como false todo vez que exigimos 
+        //o comportamento de boolean. -> se a variavel de time for 'undefined' ela vai se
+        // comportar como false -> Consequentemente !time vai se comportar como verdadeir0.
+        res.status(404).send('Não existe na série A do Brasileirão um time com essa sigla informada');
+        return;
+    }
+    res.status(200).send(time);
 });
 
 app.listen(300, () => {
